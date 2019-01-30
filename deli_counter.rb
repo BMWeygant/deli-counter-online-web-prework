da2f674d4ee036d679002ec5c616635f63c14d5b
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-01-30 17:37:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def line(array)
+  if array.empty?
+  puts "The line is currently empty."
+else
+  current_line = "The line is currently:"
+  array.each_with_index do |name, index|
+current_line << " #{index+1}. #{name}"
+end
+puts current_line
+  end
+end
+
+def take_a_number(array, string)
+  current_line = ""
+  array.each_with_index do |name, index|
+    current_line << "Welcome #{name}. You are number #{index} in line."
+end
+
+end
+
+def now_serving(array)
+
+end
